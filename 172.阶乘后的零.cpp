bd9a844c666b70302c7dc27884ff643ea1e53233
daixiongsheng <daixiongsheng@baijia.com>
2021-01-31 11:17:47 +0800

    172

--- 172.阶乘后的零.cpp
+++ 172.阶乘后的零.cpp
@@ -8,8 +8,29 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
+        // int count = 0;
+        // while(n) {
+        //     count += n /= 5;
+        // }
+        // return count;
+        int sum = 0;
+        for (int i = n; i >= 1; i--) {
+            sum += getFiveDivisor(i);
+        }
+        return sum;
+    }
 
+    int getFiveDivisor(int n) {
+        int count = 0;
+        while (n) {
+            int mod = n % 5;
+            count += mod == 0 ? 1 : 0;
+            if (mod != 0) {
+                break;
+            }
+            n /= 5;
+        }
+        return count;
     }
 };
 // @lc code=end
-
