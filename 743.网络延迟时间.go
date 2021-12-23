c4428499b490ae439909c2b55d52135ffbb4af47
daixiongsheng <daixiongsheng@gmail.com>
2021-12-24 00:52:44 +0800

    2021-12-24-00:52:44

--- 743.网络延迟时间.go
+++ 743.网络延迟时间.go
@@ -21,53 +21,45 @@ func min(a, b int) int {
 
 func networkDelayTime(times [][]int, n int, k int) int {
 	var cost = make([][]int, n)
-	var visited []bool
+	inf := 1<<31 - 1
+	var dist []int
 	for i := 0; i < n; i++ {
 		cost[i] = make([]int, n)
 		for j := 0; j < n; j++ {
-			cost[i][j] = -1
+			cost[i][j] = inf
 		}
 		cost[i][i] = 0
-		visited = append(visited, false)
+		dist = append(dist, inf)
 	}
 
 	for _, node := range times {
 		ui, vi, wi := node[0], node[1], node[2]
 		cost[ui-1][vi-1] = wi
 	}
-	sum := 0
+	dist[k-1] = 0
+	visited := make([]bool, n)
 	for i := 0; i < n; i++ {
-		f := a2b(k-1, i, n, cost, visited)
-		if f == -1 {
-			return f
-		}
-		sum = max(sum, f)
-	}
-	return sum
-}
-
-func a2b(from, to, n int, cost [][]int, visited []bool) int {
-	m := 1<<31 - 1
-	if cost[from][to] != -1 {
-		m = cost[from][to]
-	}
-	visited[from] = true
-	for i := 0; i < n; i++ {
-		if i != to && i != from && cost[from][i] != -1 && !visited[i] {
-			visited[i] = true
-			f := a2b(i, to, n, cost, visited)
-			visited[i] = false
-			if f != -1 {
-				m = min(m, f+cost[from][i])
+		m := -1
+		for j := 0; j < n; j++ {
+			if !visited[j] && (m==-1||(j != i && dist[j] < dist[m])) {
+				m = j
 			}
 		}
+		visited[m] = true
+		for idx, v := range cost[m] {
+			dist[idx] = min(dist[idx], dist[m]+v)
+		}
 	}
-	visited[from] = false
-	if m != 1<<31-1 {
-		return m
+	var ret int
+	for _, d := range dist {
+		if d == inf {
+			return -1
+		}
+		ret = max(ret, d)
 	}
-	return -1
+	return ret
 }
+
 
 // @lc code=end
 
